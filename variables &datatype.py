26341359bcc0d46ddbd11aa35bdf9b5c34948ce6
Anika Sabbir <anikasabbir26@gmail.com>
2026-03-30 23:11:28 +0600

    variables & datatypes practice

---
+++ variables &datatype.py
@@ -0,0 +1,49 @@
+# Question 1(variables)
+name="My name is Anika" 
+print(name)
+ 
+ #Question 2(variables)
+ 
+age= 24 
+city="Chattogram" 
+print(age)
+print(city)
+
+#Question 3(variables)
+
+num1=26
+num2=31
+add=num1+num2
+print(add)
+
+#Question 4(variables)
+
+age=24
+print(age) #prints old value:24
+age=25
+print(age) #prints new value:25
+
+#Question 5(variables)
+
+name="Anika"
+age=24
+city="Chattogram"
+print("My name is", name,"My age is", age,"My City is", city)
+
+
+#Question 1(data types)
+
+name="Anika" 
+age=24
+country="Bangladesh"
+print(type(name))
+print(type(age))
+print(type(country))
+
+#question 2
+name="Anika" 
+age=24
+price=20.5
+name_Anika = True
+print(name_Anika)
+print(type(name_Anika))
