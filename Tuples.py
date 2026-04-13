dacb151b53d6dea70f9819d3cc154f95412bd82d
Anika Sabbir <anikasabbir26@gmail.com>
2026-04-13 22:10:41 +0600

    Add tuples.py

--- Tuples.py
+++ Tuples.py
@@ -1,26 +1,27 @@
+#Create a tuple of 4 numbers. Print the first and second elements separately.
 tup = (2,1,5,7)
 print(tup[0])
 print(tup[1])
 
-#
+#Create a tuple with only one element. Print it and its type.
 tup=(1,)
 print(tup)
 print(type(tup))
 
-#
+#Create a tuple of 4 numbers. Print the tuple and its type
 tup=(1,2,3,4)
 print(tup)
 print(type(tup))
 
-#
+#Create a tuple of 4 numbers. Print elements from index 1 to 2 using slicing.
 tup=(1,2,3,4)
 print(tup[1:3])
 
-#
+#Create a tuple of 4 numbers. Find and print the index of value 2.
 tup=(1,2,3,4)
 print(tup.index(2))
 
-#
+#Create a tuple where 2 appears twice. Count and print how many times 2 appears
 tup=(1,2,3,2,4)
 print(tup.count(2))
 
